af02078cb65b9d20ec191b30a59866cefdc4b6f1
Avery Fay <avery@shadypixel.com>
2017-04-26 17:52:25 -0700

    convert to python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fabric_ubuntu',
+      version='0.1',
+      description='Common Ubuntu server operations as fabric tasks',
+      url='https://github.com/btmorex/fabric_ubuntu',
+      author='Avery Fay',
+      author_email='avery@shadypixel.com',
+      license='MIT',
+      packages=['fabric_ubuntu'],
+      zip_safe=False)
