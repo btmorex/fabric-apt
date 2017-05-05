202b5955550e593d5c6cf8928fbb777199b956f5
Avery Fay <avery@shadypixel.com>
2017-05-04 17:07:14 -0700

    fix uncommenting umask

--- fabric_ubuntu/user.py
+++ fabric_ubuntu/user.py
@@ -7,7 +7,8 @@ def add(user, pub_id_rsa_file):
         if exists('/home/{}'.format(user)):
             run('deluser --remove-home --quiet {}'.format(user))
         run('adduser --disabled-password --gecos "{0} user,,," --quiet {0}'.format(user))
-        uncomment('.profile', 'umask 022', backup='')
         run('mkdir --mode=0700 /home/{}/.ssh'.format(user))
         put(pub_id_rsa_file, '/home/{}/.ssh/authorized_keys'.format(user))
         run('chown --recursive {0}: /home/{0}/.ssh'.format(user))
+    with settings(user=user)
+        uncomment('.profile', 'umask 022', backup='')
