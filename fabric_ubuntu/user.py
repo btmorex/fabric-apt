7a9c06a69a4b4d6f449430afbb856f4094e52e72
Avery Fay <avery@shadypixel.com>
2017-05-15 20:40:20 -0700

    add optional --force-badname to user.add

--- fabric_ubuntu/user.py
+++ fabric_ubuntu/user.py
@@ -2,11 +2,18 @@ from fabric.api import put, run, settings, task
 from fabric.contrib.files import exists, uncomment
 
 @task
-def add(user, pub_id_rsa_file):
+def add(user, pub_id_rsa_file, force_bad_name=False):
     with settings(user='root'):
         if exists('/home/{}'.format(user)):
             run('deluser --remove-home --quiet {}'.format(user))
-        run('adduser --disabled-password --gecos "{0} user,,," --quiet {0}'.format(user))
+        adduser_flags = [
+            '--disabled-password',
+            '--gecos "{} user,,,"'.format(user),
+            '--quiet',
+        ]
+        if force_bad_name:
+            adduser_flags.append('--force-badname')
+        run('adduser {} {}'.format(' '.join(adduser_flags), user))
         run('mkdir --mode=0700 /home/{}/.ssh'.format(user))
         put(pub_id_rsa_file, '/home/{}/.ssh/authorized_keys'.format(user))
         run('chown --recursive {0}: /home/{0}/.ssh'.format(user))
