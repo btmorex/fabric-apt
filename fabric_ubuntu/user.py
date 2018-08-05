e4a76562401e5868a611a950b98fe0ab75203ed2
Avery Fay <avery@shadypixel.com>
2018-08-05 15:51:38 -0700

    don't allow bad name at all

--- fabric_ubuntu/user.py
+++ fabric_ubuntu/user.py
@@ -2,17 +2,15 @@ from fabric.api import put, run, settings, task
 from fabric.contrib.files import exists, uncomment
 
 @task
-def add(user, pub_id_rsa_file, force_bad_name=False):
+def add(user, pub_id_rsa_file):
     with settings(user='root'):
         if exists('/home/{}'.format(user)):
             run('deluser --remove-home --quiet {}'.format(user))
-        adduser_flags = [
+        adduser_flags = (
             '--disabled-password',
             '--gecos "{} user,,,"'.format(user),
             '--quiet',
-        ]
-        if force_bad_name:
-            adduser_flags.append('--force-badname')
+        )
         run('adduser {} {}'.format(' '.join(adduser_flags), user))
         run('mkdir --mode=0700 /home/{}/.ssh'.format(user))
         put(pub_id_rsa_file, '/home/{}/.ssh/authorized_keys'.format(user))
